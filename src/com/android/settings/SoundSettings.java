228996d7a9e450274984dbac58511f5e5dd5be9c
thanushyon <thanushyon@thanushyon.com>
2015-04-19 22:37:12 +0100

    Revert "Fix importing of ListPrefence"
    This reverts commit 5a681cd204a6442a4fd92c061518b5c2cf82464b.

--- src/com/android/settings/SoundSettings.java
+++ src/com/android/settings/SoundSettings.java
@@ -45,7 +45,6 @@ import android.preference.PreferenceScreen;
 import android.preference.SeekBarVolumizer;
 import android.preference.SwitchPreference;
 import android.preference.TwoStatePreference;
-import android.preference.ListPreference;
 import android.provider.MediaStore;
 import android.provider.OpenableColumns;
 import android.provider.SearchIndexableResource;
