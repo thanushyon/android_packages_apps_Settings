24d6d7e8ed46357994c03a011087138e8aa8939c
thanushyon <thanushyon@thanushyon.com>
2015-04-19 23:47:17 +0100

    Implement less notifications features

--- src/com/android/settings/notification/OtherSoundSettings.java
+++ src/com/android/settings/notification/OtherSoundSettings.java
@@ -32,6 +32,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Vibrator;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
@@ -39,6 +40,7 @@ import android.provider.SearchIndexableResource;
 import android.provider.Settings.Global;
 import android.provider.Settings.System;
 import android.telephony.TelephonyManager;
+import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -51,7 +53,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class OtherSoundSettings extends SettingsPreferenceFragment implements Indexable {
+public class OtherSoundSettings extends SettingsPreferenceFragment implements Indexable,
+                Preference.OnPreferenceChangeListener {
     private static final String TAG = "OtherSoundSettings";
 
     private static final int DEFAULT_ON = 1;
@@ -76,6 +79,7 @@ public class OtherSoundSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_POWER_NOTIFICATIONS = "power_notifications";
     private static final String KEY_POWER_NOTIFICATIONS_VIBRATE = "power_notifications_vibrate";
     private static final String KEY_POWER_NOTIFICATIONS_RINGTONE = "power_notifications_ringtone";
+    private static final String KEY_LESS_NOTIFICATION_SOUNDS = "less_notification_sounds";
 
     // Request code for power notification ringtone picker
     private static final int REQUEST_CODE_POWER_NOTIFICATIONS_RINGTONE = 1;
@@ -86,6 +90,7 @@ public class OtherSoundSettings extends SettingsPreferenceFragment implements In
     private SwitchPreference mPowerSounds;
     private SwitchPreference mPowerSoundsVibrate;
     private Preference mPowerSoundsRingtone;
+    private ListPreference mAnnoyingNotifications;
 
     private static final SettingPref PREF_DIAL_PAD_TONES = new SettingPref(
             TYPE_SYSTEM, KEY_DIAL_PAD_TONES, System.DTMF_TONE_WHEN_DIALING, DEFAULT_ON) {
@@ -196,6 +201,13 @@ public class OtherSoundSettings extends SettingsPreferenceFragment implements In
 
         mContext = getActivity();
 
+
+        mAnnoyingNotifications = (ListPreference) findPreference(KEY_LESS_NOTIFICATION_SOUNDS);
+        int notificationThreshold = Settings.System.getInt(getContentResolver(),
+                Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, 0);
+        updateAnnoyingNotificationValues();
+
+
         // power state change notification sounds
         mPowerSounds = (SwitchPreference) findPreference(KEY_POWER_NOTIFICATIONS);
         mPowerSounds.setChecked(Global.getInt(getContentResolver(),
@@ -235,6 +247,18 @@ public class OtherSoundSettings extends SettingsPreferenceFragment implements In
         }
     }
 
+    private void updateAnnoyingNotificationValues() {
+        int notificationThreshold = Settings.System.getInt(getContentResolver(),
+                Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, 0);
+        if (mAnnoyingNotifications == null) {
+            mAnnoyingNotifications.setSummary(getString(R.string.less_notification_sounds_summary));
+        } else {
+            mAnnoyingNotifications.setValue(Integer.toString(notificationThreshold));
+            mAnnoyingNotifications.setSummary(mAnnoyingNotifications.getEntry());
+            mAnnoyingNotifications.setOnPreferenceChangeListener(this);
+        }
+    }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -246,6 +270,19 @@ public class OtherSoundSettings extends SettingsPreferenceFragment implements In
         super.onPause();
         mSettingsObserver.register(false);
     }
+
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final String key = preference.getKey();
+        if (KEY_LESS_NOTIFICATION_SOUNDS.equals(key)) {
+            final int val = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, val);
+            updateAnnoyingNotificationValues();
+        }
+        return true;
+    }
+
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
